7d9a8a7799442c0d154a6030ebaa1d0adf0e59fb
artempelevin <artempelevin@list.ru>
2022-10-30 13:11:42 +0300

    refactor: add / to the end of urls

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from users.views import SignUp, SignIn, SignOut
 
 urlpatterns = [
-    path('signin', SignIn.as_view(), name='signin'),
-    path('signout', SignOut.as_view(), name='signout'),
-    path('signup', SignUp.as_view(), name='signup')
+    path('signin/', SignIn.as_view(), name='signin'),
+    path('signout/', SignOut.as_view(), name='signout'),
+    path('signup/', SignUp.as_view(), name='signup')
 ]
