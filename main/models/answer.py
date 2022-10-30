76474b93221ac20cb47e1d6fbe2e520ac13345f9
artempelevin <artempelevin@list.ru>
2022-10-30 13:09:42 +0300

    fix: verbose text with error

--- main/models/answer.py
+++ main/models/answer.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class Answer(models.Model):
-    text = models.TextField(null=False, verbose_name='Тест ответа')
+    text = models.TextField(null=False, verbose_name='Текст ответа')
     is_right = models.BooleanField(null=False, default=False, verbose_name='Это правильный ответ?')
     question = models.ForeignKey('Question', on_delete=models.CASCADE, null=False)
-
-    def __str__(self):
-        return self.text
 
     class Meta:
         app_label = 'main'
         db_table = 'answers'
         verbose_name = 'ответ'
         verbose_name_plural = 'ответы'
+
+    def __str__(self):
+        return self.text
--- main/models/question.py
+++ main/models/question.py
@@ -2,13 +2,14 @@ from django.db import models
 
 
 class Question(models.Model):
-    text = models.TextField(null=False, verbose_name='Тест вопроса')
-
-    def __str__(self):
-        return self.text
+    text = models.TextField(null=False, verbose_name='Текст вопроса')
+    test = models.ForeignKey('Test', on_delete=models.CASCADE)
 
     class Meta:
         app_label = 'main'
         db_table = 'questions'
         verbose_name = 'вопрос'
         verbose_name_plural = 'вопросы'
+
+    def __str__(self):
+        return self.text
