c0f45ec297b3d2ea85e436ea0e95445c15cf8ace
artempelevin <artempelevin@list.ru>
2022-11-01 22:22:13 +0300

    feat: configured auth system

--- testing_system/settings.py
+++ testing_system/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -26,7 +25,12 @@ SECRET_KEY = 'django-insecure-w9+g1sfvy6(_1818qaf#lkyk_c#y(@x=756^w92l-yj!af*roz
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-LOGIN_REDIRECT_URL = '/users/signin/'
+
+LOGIN_URL = '/users/signin/'
+LOGIN_REDIRECT_URL = LOGIN_URL
+LOGOUT_URL = LOGIN_URL
+LOGOUT_REDIRECT_URL = LOGOUT_URL
+
 
 # Application definition
 
@@ -57,7 +61,7 @@ ROOT_URLCONF = 'testing_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'testing_system.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -86,7 +89,6 @@ DATABASES = {
         'PORT': '5432',
     },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -106,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -117,7 +118,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
