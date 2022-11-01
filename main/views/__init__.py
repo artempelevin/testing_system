7ac69cf08b74badd45a2e05a9ae7378b3e4e896b
artempelevin <artempelevin@list.ru>
2022-11-02 00:11:28 +0300

    refactor: import from init file for views

---
+++ main/views/__init__.py
@@ -0,0 +1,11 @@
+from .start import start_page_view
+from .test import QuestionsListView
+from .test_suite import TestsListView
+from .test_suites import TestSuiteListView
+
+__all__ = [
+    'start_page_view',
+    'QuestionsListView',
+    'TestsListView',
+    'TestSuiteListView',
+]
